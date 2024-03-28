e24af52fd26085e968c5267722aba214dfcc0c95
Jose Muñoz <jmunozlo.dev@gmail.com>
2024-03-27 22:25:20 -0300

    Initial commit

---
+++ src/user/user.service.ts
@@ -0,0 +1,33 @@
+import { HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UserService {
+  private users: User[] = [
+    { id: 1, name: 'Gino', age: 36 },
+    { id: 2, name: 'Jose', age: 35 },
+  ];
+  create(createUserDto: CreateUserDto) {
+    return 'This action adds a new user';
+  }
+
+  findAll(): User[] {
+    return this.users;
+  }
+
+  findOne(id: number): User {
+    const user = this.users.find((user) => user.id === id);
+    if (!user) throw new NotFoundException('Este wn no esta', 'Test');
+    return user;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
